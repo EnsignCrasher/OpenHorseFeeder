6cfe316c36cfcf9c451e7cf065e1aff065271869
Julian Heuking <J.Heuking@beckhoff.com>
2021-07-15 21:47:04 +0200

    arduino: implement auto-address for i2c

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -1,3 +1,7 @@
+#include <string.h>
+#include <stdlib.h>     /* atoi */
+
+
 /*
   Arduino Slave for Raspberry Pi Master
   i2c_slave_ard.ino
@@ -6,6 +10,47 @@
   DroneBot Workshop 2019
   https://dronebotworkshop.com
 */
+int pos = 1;
+
+void getline(char * buffer)
+{
+  uint8_t idx = 0;
+  char c;
+  buffer[0] = 0;
+  do
+  {
+    while (Serial.available() == 0) ; // wait for a char this causes the blocking
+    c = Serial.read();
+    buffer[idx++] = c;
+  }
+  while (c != '\n' && c != '\r');
+  buffer[idx] = 0;
+}
+
+void wait_for_address()
+{
+  char buf[128];
+  while(millis() < 5000) {
+    while(Serial.available()) {
+      getline(buf);
+      if (strncmp(buf, "pos: ", 5) == 0) {
+        int new_pos = atoi(&buf[4]) + 1;
+        if (pos != new_pos) {
+          pos = new_pos;
+          Serial.print("Setting new position to ");
+          Serial.println(pos);
+        }
+      }
+      Serial.print("received: ");
+      Serial.println(buf);
+    }
+    Serial.print("pos: ");
+    Serial.println(pos);
+    delay(100);
+  }
+  Serial.print("Starting I2C at address ");
+  Serial.println(pos);
+}
 
 // Include the Wire library for I2C
 #include <Wire.h>
@@ -18,12 +63,8 @@ const int fetPin = 10;
 const int btnPin = 2;
 
 void setup() {
-  // Join I2C bus as slave with address 8
-  Wire.begin(0x8);
-
-  // Call receiveEvent when data received
-  Wire.onReceive(receiveEvent);
-  Wire.onRequest(requestEvent);
+  Serial.begin(9600);
+  Serial.println("start");
 
   // Setup pin 13 as output and turn LED off
   pinMode(ledPin, OUTPUT);
@@ -31,6 +72,14 @@ void setup() {
   pinMode(btnPin, INPUT_PULLUP);
   digitalWrite(ledPin, LOW);
   digitalWrite(fetPin, LOW);
+
+  wait_for_address();
+
+  Wire.begin(pos);
+
+  // Call receiveEvent when data received
+  Wire.onReceive(receiveEvent);
+  Wire.onRequest(requestEvent);
 }
 
 // Function that executes whenever data is received from master
