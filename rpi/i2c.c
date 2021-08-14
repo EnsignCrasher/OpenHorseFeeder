785914f2d52852a73484191785d972a9c66c93e2
Julian Heuking <J.Heuking@beckhoff.com>
2021-08-14 17:54:07 +0200

    rpi/i2c: not an easy task to code without any warning

--- rpi/i2c.c
+++ rpi/i2c.c
@@ -20,18 +20,19 @@ int i2c_init(int addr) {
 		printf("Failed to acquire bus access and/or talk to slave.\n");
 		return 1;
 	}
+	return 0;
 }
 
 int i2c_read(int length, char *buffer) {
 	//read() returns the number of bytes actually read, if it doesn't match
 	//then an error occurred (e.g. no response from the device)
-	if (read(file_i2c, buffer, length) != length)
+	int read_len=read(file_i2c, buffer, length);
+	if (read_len != length)
 	{
-		printf("Failed to read from the i2c bus.\n");
+		printf("Failed to read from the i2c bus. Got %d bytes, wanted %d\n", read_len, length);
 		return 1;
 	}
-
-
+	return 0;
 }
 
 int i2c_write(int length, char* buffer) {
@@ -42,6 +43,7 @@ int i2c_write(int length, char* buffer) {
 		printf("Failed to write to the i2c bus.\n");
 		return 1;
 	}
+	return 0;
 }
 
 int usage() {
@@ -57,21 +59,30 @@ int main(int argc, char* argv[]) {
 	if (0 == strcmp(argv[1], "get")) {
 		if (argc <= 2) return usage();
 		int addr = strtol(argv[2], NULL, 10);
-		i2c_init(addr);
+		if(i2c_init(addr)) {
+			return 1;
+		}
 		char value = 0;
-		i2c_read(1, &value);
-		printf("%hhu\n", value);
+		if(i2c_read(1, &value)) {
+			return 1;
+		}
+		printf("%d\n", value);
 	} else if (0 == strcmp(argv[1], "set")) {
 		if (argc <= 3) return usage();
 
 		int addr = strtol(argv[2], NULL, 10);
-		i2c_init(addr);
+		if(i2c_init(addr)) {
+			return 1;
+		}
 
 		int val = strtol(argv[3], NULL, 10);
 		char cval = (char)val;
-		i2c_write(1, &cval);
+		if(i2c_write(1, &cval)) {
+			return 1;
+		}
 	} else {
 		printf("unknown mode %s\n", argv[1]);
 		return 1;
 	}
+	return 0;
 }
