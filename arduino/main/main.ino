f68609c97f4bcfbd4bb6aa29669c805abc1634ce
Julian Heuking <J.Heuking@beckhoff.com>
2022-02-19 14:31:16 +0100

    Arduino; make autopositioning work

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -10,46 +10,18 @@
   DroneBot Workshop 2019
   https://dronebotworkshop.com
 */
-int pos = 1;
-
-void getline(char * buffer)
-{
-  uint8_t idx = 0;
-  char c;
-  buffer[0] = 0;
-  do
-  {
-    while (Serial.available() == 0) ; // wait for a char this causes the blocking
-    c = Serial.read();
-    buffer[idx++] = c;
-  }
-  while (c != '\n' && c != '\r');
-  buffer[idx] = 0;
-}
+byte pos = 48;
 
 void wait_for_address()
 {
-  char buf[128];
+  byte new_pos = pos;
   while(millis() < 5000) {
     while(Serial.available()) {
-      getline(buf);
-      if (strncmp(buf, "pos: ", 5) == 0) {
-        int new_pos = atoi(&buf[4]) + 1;
-        if (pos != new_pos) {
-          pos = new_pos;
-          Serial.print("Setting new position to ");
-          Serial.println(pos);
-        }
-      }
-      Serial.print("received: ");
-      Serial.println(buf);
+      pos = Serial.read() + 1;
     }
-    Serial.print("pos: ");
-    Serial.println(pos);
+    Serial.write(pos);
     delay(100);
   }
-  Serial.print("Starting I2C at address ");
-  Serial.println(pos);
 }
 
 // Include the Wire library for I2C
@@ -64,7 +36,6 @@ const int btnPin = 2;
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("start");
 
   // Setup pin 13 as output and turn LED off
   pinMode(ledPin, OUTPUT);
